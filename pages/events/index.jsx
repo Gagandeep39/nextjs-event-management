f6eb64427c93df5cdbfa84748036a04d6a2a114a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-07-02 22:01:00 +0530

    Aded all event logic

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,7 +1,12 @@
+import EventList from '../../components/events/event-list.jsx';
+import { getAllEvents } from '../data/dummy-data.js';
+
 const AllEvents = () => {
+  const allEvents = getAllEvents();
+
   return (
     <div>
-      <h1>Events</h1>
+      <EventList items={allEvents} />
     </div>
   );
 };
