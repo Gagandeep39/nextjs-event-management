5c81e479d0e902c09c6cc0488e4438e236e766ff
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-07-02 12:38:36 +0530

    Added compoennts in home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,13 @@
+import EventList from '../components/events/event-list.jsx';
+import { getFeaturedEvents } from './data/dummy-data.js';
+
 export default function Home() {
-  return <div>Home</div>;
+  const featuredEvents = getFeaturedEvents();
+  console.log(featuredEvents);
+  return (
+    <div>
+      <h1>Home</h1>
+      <EventList items={featuredEvents} />
+    </div>
+  );
 }
